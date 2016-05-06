ccf242f5b59df2c3fb25ab96e0d26efeffc72786
Towardthesea <ph17dn@gmail.com>
2016-05-06 11:43:47 +0200

    Clean plannerModule.cpp

--- src/plannerModule.cpp
+++ src/plannerModule.cpp
@@ -65,50 +65,9 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-//    // Test=======================================================
-//    Vector workspace(6,0.0);
-//    workspace[3]=30.0/10.0;
-//    workspace[4]=30.0/10.0;
-//    workspace[5]=30.0/10.0;
-
-//    double scale = 10.0;
-//    double sizeObject = 1.0/scale;  //1.0/10.0;
-//    double sizeGoal = .15; //1.4/scale;
-
-//    Vector goal(6,sizeGoal);
-//    goal[0]=-1.0/scale; //-3.0/scale; //-10.0/scale;
-//    goal[1]=5.64/scale; //6.0/scale; //7.0/scale;
-//    goal[2]=3.7/scale;  //1.0/scale;
-
-//    srand(time(NULL));
-//    vector<Vector> obsSet;
-//    for (int i=0; i<=10; i++)
-//    {
-//        Vector obs(6, sizeObject);
-//        obs[0] = (double)(rand()%8-4)/scale;
-//        //obs[1] = 0.564;
-//        obs[1] = 0.614;
-//        obs[4] = 0.2;
-//        obs[2] = (double)(rand()%4+3)/scale;
-//        if ((abs(10*obs[0]-10*goal[0])>=abs(10*obs[3]-10*goal[3]))&&
-//                (abs(10*obs[2]-10*goal[2])>=abs(10*obs[5]-10*goal[5]))&&
-//                (abs(10*obs[2]-10*goal[2])>=abs(10*obs[5]-10*goal[5])))
-//        {
-//            obsSet.push_back(obs);
-//        }
-
-//    }
-//    //============================================================
 
     upperPlanner planner;
     planner.configure(rf);
-
-//    planner.setRegionOperating(workspace);
-//    planner.setGoal(goal);
-//    planner.setDeadline(0.65);
-//    planner.setObstacles(obsSet);
-
-
 
     return planner.runModule();
     //return planner.runModule(rf);   // runModule(rf) = configure() + runModule()
